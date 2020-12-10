1aee2c2c58c3db1f165552c9687d07c94b6ded0a
Cong Nguyen Thanh <thanhcong.bk10@gmail.com>
2020-12-10 16:34:32 +0700

    Update command

--- source/run.py
+++ source/run.py
@@ -2,138 +2,114 @@ import os
 import pathlib
 import getopt
 import sys
+import subprocess
 
 
-def is_valid_idl_file(idl_file_content):
-    if idl_file_content != "":
-        f = open("check_Messenger.idl", 'w')
-        #Convert
-        f.write(idl_file_content)
-        f.close()
+def is_valid_idl_file(idl_path):
+    if not os.path.isfile(idl_path):
+        raise SystemExit(f"The idl file in {idl_path} isn't existed")
 
     #Add command
-
-
+    try:
+        subprocess.check_output(f"opendds_idl --syntax-only {idl_path}", shell=True)
+    except subprocess.CalledProcessError:
+        return False
     return True
 
-def build(idl_file_content=None):
+def build(action, idl_path):
     # 1. Prepare a temporary directory to build
     # make temp folder to build source in ${PWD}/build
+    if action == "publisher":
+        child_folder = "publisher"
+    else:
+        child_folder = "subscriber"
+
     cwd = os.getcwd()
     root_path = pathlib.Path(pathlib.Path(__file__).parent.absolute()).parent
-    build_path = os.path.join(root_path, "build")
-    template_path = os.path.join(root_path, "template")
+    build_path = os.path.join(root_path, f".{child_folder}")
+    template_path = os.path.join(root_path, "template",child_folder)
 
     if os.path.isdir(build_path):
-        command = "rm -rf {}"
-        command = command.format(build_path)
-        os.system(command)
-    command = "mkdir {}"
-    command = command.format(build_path)
-    os.system(command)
+        os.system(f"rm -rf {build_path}")
+    os.system(f"mkdir {build_path}")
+    os.system(f"cp {template_path}/* {build_path}")
 
-
-    command = "cp {}/* {}"
-    command = command.format(template_path, build_path)
-    os.system(command)
+    if not os.path.isfile(idl_path):
+        raise SystemExit(f"The idl file in {idl_path} isn't existed")
 
     # 2. build
     os.chdir(build_path)
-    if type(idl_file_content) is str:
-        f = open("Messenger.idl", 'w')
-        #Convert
-        f.write(idl_file_content)
-        f.close()
-
-    command = "cmake {}"
-    command = command.format(build_path)
-    os.system(command)
-
-    command = "cmake --build ."
-    command = command.format(build_path)
-    os.system(command)
-
+    os.system(f"cp {idl_path} Messenger.idl")
+    os.system("cmake .")
+    os.system("cmake --build .")
     os.chdir(cwd)
 
-def run(net_config_file_content = None, isPublic = None):
+def run(action, net_config_path):
+    if action == "publisher":
+        node_name = "publisher"
+    else:
+        node_name = "subscriber"
+
     root_path = pathlib.Path(pathlib.Path(__file__).parent.absolute()).parent
-    build_path = os.path.join(root_path, "build")
+    build_path = os.path.join(root_path, f".{node_name}")
 
     rtps_file = "relay_rtps.ini"
-    if type(net_config_file_content) is str:
-        f = open("{}/rtps.ini".format(build_path), 'w')
-        #Convert
-        f.write(net_config_file_content)
-        f.close()
-        rtps_file = "rtps.ini"
+    if not os.path.isfile(net_config_path):
+        raise SystemExit(f"The config file in {net_config_path} isn't existed")
 
-    node_name = "subscriber"
+    if not os.path.isfile(f"{build_path}/{node_name}"):
+        raise SystemExit(f"The execution file in {build_path}/{node_name} isn't existed. Please try to build it before!")
 
-    if isPublic:
-        node_name = "publisher"
+    os.system(f"{build_path}/{node_name} -DCPSConfigFile {net_config_path}")
 
-    command = "{}/{} -DCPSConfigFile {}/{}"
-    command = command.format(build_path,node_name,build_path,rtps_file)   
-    os.system(command)
-
-def public_example():
+def publish_example():
     root_path = pathlib.Path(pathlib.Path(__file__).parent.absolute()).parent
-    build_path = os.path.join(root_path, "build")
-    if not os.path.isfile("{}/publisher".format(build_path)):
-        build()
-    run(isPublic=True)
-
+    template_path = os.path.join(root_path, "template")
+    idl_path = f"{template_path}/Messenger.idl"
+    net_config_path = f"{template_path}/relay_rtps.ini"
+    build("publisher", idl_path)
+    run("publisher", net_config_path)
     
-
 def parse(argvs, short_opt, long_opt):
     options, arguments = getopt.getopt(
-        argvs,                      # Arguments
-        short_opt,                            # Short option definitions
-        long_opt) # Long option definitions
-    separator = "\n"
+        argvs,              # Arguments
+        short_opt,          # Short option definitions
+        long_opt)           # Long option definitions
     result = list()
     for o, a in options:
-        if o in ("-n", "--netConfig"):
+        if o in ("-t", "--type"):
             result += [a]
-        if o in ("-i", "--idl"):
+        elif o in ("-n", "--netConfig"):
             result += [a]
-    # if not arguments or len(arguments) > 3:
-    #     raise SystemExit(USAGE)
-    # try:
-    #     operands = [int(arg) for arg in arguments]
-    # except ValueError:
-    #     raise SystemExit(USAGE)
+        elif o in ("-i", "--idl"):
+            result += [a]
     return result
 
+USAGE = f'''Usage: 
+                python3 {sys.argv[0]} check [-i | --idl ] 
+                python3 {sys.argv[0]} build [-t | --type] [-i | --idl ]
+                python3 {sys.argv[0]} run   [-t | --type] [-n | --netConfig ]
+                python3 {sys.argv[0]} publish_example'''
+
 def main():
-
-    USAGE = f'''Usage: 
-                    python3 {sys.argv[0]} check [-i | --idl ] 
-                    python3 {sys.argv[0]} build [-i | --idl ]
-                    python3 {sys.argv[0]} run   [-n | --netConfig ] [--] [-i | --idl]
-                    python3 {sys.argv[0]} public
-                    python3 {sys.argv[0]}'''
-
     if (len(sys.argv) > 1):
         if sys.argv[1] == "check":
-            idl = parse(sys.argv[2:], 'i', ["idl"])[0]
-            is_valid_idl_file(idl)
+            idl_path = parse(sys.argv[2:], 'i:', ["idl="])[0]
+            is_valid_idl_file(idl_path)
         elif sys.argv[1] == "build":
-            idl_path = parse(sys.argv[2:], 'i', ["idl"])[0]
-            build(idl)
+            result = parse(sys.argv[2:], 't:i:', ["type=", "idl="])
+            build(result[0], result[1])
         elif sys.argv[1] == "run":
-            result = parse(sys.argv[2:], 'ni:', ["netConfig", "idl="])
-            if len(result) == 2:
-                build(result[1])
-            run(result[0])
-        elif sys.argv[1] == "public":
-            public_example()
+            result = parse(sys.argv[2:], 't:n:', ["type=", "netConfig="])
+            run(result[0], result[1])
+        elif sys.argv[1] == "publish_example":
+            publish_example()
         else:
             print(sys.argv)
             print(USAGE)
     else:
-        build()
-        run()
+        print(sys.argv)
+        print(USAGE)
 
 if __name__ == '__main__':
     main()
